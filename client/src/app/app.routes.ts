adb58dcb8c25354a766644eeefd6abe919bb5ea0
Gurthage Sall <gurthages@gmail.com>
2024-08-28 09:59:24 +1000

    Fixed the route problem

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,17 +2,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-//import { AdminComponent } from './admin/admin.component';
-//import { SuperAdminComponent } from './super-admin/super-admin.component';
-//import { ChatComponent } from './chat/chat.component';
+// import { AdminComponent } from './admin/admin.component';
+// import { SuperAdminComponent } from './super-admin/super-admin.component';
+// import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
- // { path: 'admin', component: AdminComponent },
- // { path: 'super-admin', component: SuperAdminComponent },
- // { path: 'chat', component: ChatComponent },
+  { path: 'register', component: RegisterComponent },
+  // { path: 'admin', component: AdminComponent },
+  // { path: 'super-admin', component: SuperAdminComponent },
+  // { path: 'chat', component: ChatComponent },
 ];
 
+// Export the routes so they can be used in app.config.ts
 export const routing = RouterModule.forRoot(routes);
